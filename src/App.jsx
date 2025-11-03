863d1f1faa1c60b87130e02e6a1c981d3d9ca9ff
Caleb Morse <calebmorse.code@gmail.com>
2025-11-02 19:04:53 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import UsersList from './UsersList';
+
+function App() {
+  return (
+    <>
+      <h1>React Playground</h1>
+      <UsersList />
+    </>
+  );
+}
+
+export default App;
---
+++ src/UsersList.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+function UsersList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users',
+        );
+        const data = await response.json();
+
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+  console.log(users);
+  return (
+    <>
+      <div>
+        <h2>List of Users</h2>
+        <div className="contact-grid">
+          {users.map((user) => (
+            <div className="contact-card" key={user.id}>
+              <h4>{user.name}</h4>
+              <p>Company: {user.company.name}</p>
+              <p>Email: {user.email}</p>
+              <p>Phone: {user.phone}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UsersList;
